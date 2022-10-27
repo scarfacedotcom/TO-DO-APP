3b1e72fb048bc1461f65f92e8a4bc9a471d3d446
Scar-Face <peteradaaku@gmail.com>
2022-10-27 15:36:02 +0100

    add list

---
+++ index.js
@@ -0,0 +1,29 @@
+const addForm = document.querySelector('.add')
+const list = document.querySelector('.todos')
+
+const newTemplate = (Todo) => {
+   
+   const html = `<li
+   class="list-group-item d-flex justify-content-between align-items-center"
+ >
+   <span>${Todo}</span>
+   <i class="far fa-trash-alt delete"></i>
+ </li>` ;
+
+ list.innerHTML += html
+}
+
+addForm.addEventListener('submit', e => {
+
+    e.preventDefault()
+    const Todo = addForm.add.value.trim();
+
+    if(Todo.length) {
+      newTemplate(Todo)
+      addForm.reset()
+    }
+})
+
+list.addEventListener('click', e => {
+    
+})
